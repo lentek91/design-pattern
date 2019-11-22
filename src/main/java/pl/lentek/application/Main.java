f5d98c8f6b78dfdcf57c1cd538914091e833f75d
Lentek <klentowicz91@gmail.com>
2019-11-22 17:45:38 +0100

    Przygotowanie projektu gradle

--- src/main/java/pl/lentek/application/Main.java
+++ src/main/java/pl/lentek/application/Main.java
@@ -1,7 +1,6 @@
-package main.java.pl.lentek.application;
+package pl.lentek.application;
 
 public class Main {
-
   public static void main(String[] args) {
     System.out.println(
         "There is a master branch for design pattern training in JAVA. Pattern are available in branches.");
