93fb3f9141c14bac1525b3a3a9a81d5277ef2904
Lentek <klentowicz91@gmail.com>
2019-11-22 16:13:08 +0100

    Create Main class on master

--- src/main/java/pl/lentek/application/Main.java
+++ src/main/java/pl/lentek/application/Main.java
@@ -1,24 +1,9 @@
 package main.java.pl.lentek.application;
 
-import main.java.pl.lentek.entity.WeatherForecast;
-import main.java.pl.lentek.observer.InternetNews;
-import main.java.pl.lentek.observer.RadioNews;
-import main.java.pl.lentek.observer.TvNews;
-
 public class Main {
 
   public static void main(String[] args) {
-    WeatherForecast weatherForecast = new WeatherForecast(25, 1003);
-    RadioNews radioNews = new RadioNews();
-    InternetNews internetNews = new InternetNews();
-    TvNews tvNews = new TvNews();
-    weatherForecast.registerObserver(radioNews);
-    weatherForecast.registerObserver(internetNews);
-    weatherForecast.registerObserver(tvNews);
-    weatherForecast.notifyObservers();
-    weatherForecast.unRegisterObserver(tvNews);
-    weatherForecast.unRegisterObserver(radioNews);
-    System.out.println("Nowa prognoza - powiadomienie tylko dla serwisu internetowego:");
-    weatherForecast.updateForecast(18, 1007);
+    System.out.println(
+        "There is a master branch for design pattern training in JAVA. Pattern are available in branches.");
   }
 }
--- src/main/java/pl/lentek/entity/WeatherForecastObserver.java
+++
@@ -1,8 +0,0 @@
-package main.java.pl.lentek.entity;
-
-import main.java.pl.lentek.entity.WeatherForecast;
-
-public interface WeatherForecastObserver {
-
-  void updateForecast(WeatherForecast weatherForecast);
-}
