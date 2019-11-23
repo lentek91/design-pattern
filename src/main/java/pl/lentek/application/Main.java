b5ff82659c978a845ef82fbef73759b5e15b2e6c
Lentek <klentowicz91@gmail.com>
2019-11-23 14:14:27 +0100

    Adapter

--- src/main/java/pl/lentek/application/Main.java
+++ src/main/java/pl/lentek/application/Main.java
@@ -10,10 +10,10 @@ public class Main {
 
   public static void main(String[] args) {
 
-    User user = new User("Paweł","Cwik","32131212");
+    User user = new User("Paweł", "Cwik", "32131212");
     LibraryAPIv2 libraryAPIv2 = new LibraryAPIv2Impl();
     APIAdapter apiAdapter = new APIAdapter(libraryAPIv2, user);
-    BookConnector connector = new BookConnector(user,apiAdapter);
+    BookConnector connector = new BookConnector(user, apiAdapter);
     connector.checkAvailability("Harry Potter i Zakon Feniksa");
   }
 }
